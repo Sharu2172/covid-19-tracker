296f99b309da64e996b4544ac7969d3594d3208d
D.Sharath <57060263+Sharu2172@users.noreply.github.com>
2021-10-30 18:10:12 +0530

    deployed

---
+++ build/precache-manifest.9558e348bd885bdefeea1bde80bae75e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4c7a56b7d95510aed03c57923c28ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "1ca3846f4d1af724efc7",
+    "url": "/static/css/main.1220586b.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca3846f4d1af724efc7",
+    "url": "/static/js/main.af7ba9c3.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
--- src/util.js
+++ src/util.js
@@ -7,19 +7,19 @@ const casesTypeColors = {
     hex: "#CC1034",
     rgb: "rgb(204, 16, 52)",
     half_op: "rgba(204, 16, 52, 0.5)",
-    multiplier: 800,
+    multiplier: 200,
   },
   recovered: {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
     half_op: "rgba(125, 215, 29, 0.5)",
-    multiplier: 1200,
+    multiplier: 200,
   },
   deaths: {
     hex: "#fb4443",
     rgb: "rgb(251, 68, 67)",
     half_op: "rgba(251, 68, 67, 0.5)",
-    multiplier: 2000,
+    multiplier: 200,
   },
 };
 
@@ -44,7 +44,7 @@ export const showDataOnMap = (data, casesType = "cases") =>
       center={[country.countryInfo.lat, country.countryInfo.long]}
       color={casesTypeColors[casesType].hex}
       fillColor={casesTypeColors[casesType].hex}
-      fillOpacity={0.4}
+      fillOpacity={0.2}
       radius={
         Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
       }
